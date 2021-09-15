e0cd54a49683010e5eeca975c0f3cfb8f410be8b
STATIC <1289022116@qq.com>
2021-09-15 20:05:00 +0800

    初始化

---
+++ springboot-ddd/src/test/java/com/zcj/ddd/SpringbootDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcj.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
